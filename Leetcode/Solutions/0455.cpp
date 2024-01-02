4cf03a0d264e997e41696410b2a0976486dea1a3
jayp5545 <110543100+jayp5545@users.noreply.github.com>
2024-01-02 04:02:58 +0400

    Added solution for Assign Cookies

---
+++ Leetcode/Solutions/0455.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        int j=0;
+        int cnt=0;
+        while(i<g.size() && j<s.size()){
+            if(s[j] >= g[i]){
+                i++;
+                cnt++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+};
